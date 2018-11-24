51d44a0f74aea385f008f2896fac7b5e442ef954
Matthew Wu <mewu@caltech.edu>
2018-11-24 12:54:18 -0800

    Fix link test

--- link_test.py
+++ link_test.py
@@ -1,5 +1,8 @@
-from components import Link, Packet, Flow
 from events import EventManager
+from host import Host
+from link import Link
+from flow import Flow
+from packet import Packet, DATA_PACKET_SIZE, CONTROL_PACKET_SIZE
 
 class MockHost:
     def __init__(self, i):
@@ -7,19 +10,26 @@ class MockHost:
         self.times = []
         self.packets = []
 
-    def on_packet_reception(self, t, p):
+    def on_reception(self, t, p):
         self.times.append(t)
         self.packets.append(p)
+
+class MockFlow:
+    def __init__(self, i):
+        self.i = i
 
 em = EventManager()
 h1 = MockHost('H1')
 h2 = MockHost('H2')
-f = Flow(em, 'F', h1, h2, 100)
-l = Link(em, 'L', h1, h2, 1, 10, 100)  # rate 1, delay 10
-l.on_packet_entry(0, Packet('P1', h1, h2, f, 0, 1))
-l.on_packet_entry(0, Packet('P2', h1, h2, f, 0, 1))
-l.on_packet_entry(0, Packet('P3', h1, h2, f, 0, 1))
-l.on_packet_entry(0, Packet('P4', h1, h2, f, 0, 1))
-l.on_packet_entry(0, Packet('P5', h1, h2, f, 0, 1))
+f = MockFlow('F')
+# rate 1 pkt/sec, delay 10s
+l = Link(em, 'L', h1, h2, DATA_PACKET_SIZE, 10, DATA_PACKET_SIZE * 10, debug=False)
+l.on_packet_entry(0, Packet('P1', f, h1, h2, False, False, False, 1, 0, DATA_PACKET_SIZE))
+l.on_packet_entry(0, Packet('P2', f, h1, h2, False, False, False, 2, 0, DATA_PACKET_SIZE))
+l.on_packet_entry(0, Packet('P3', f, h1, h2, False, False, False, 3, 0, DATA_PACKET_SIZE))
+l.on_packet_entry(0, Packet('P4', f, h1, h2, False, False, False, 4, 0, DATA_PACKET_SIZE))
+l.on_packet_entry(0, Packet('P5', f, h1, h2, False, False, False, 5, 0, DATA_PACKET_SIZE))
+# print(l.buffer)
+# print(h2.packets, h2.times)
 em.run()
 assert h2.times == [11, 12, 13, 14, 15]
