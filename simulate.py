7902f80a10d992dc6018cd2c48804fabaeb797ba
Matthew Wu <mewu@caltech.edu>
2018-12-02 01:29:27 -0800

    Add simulate script

---
+++ simulate.py
@@ -0,0 +1,18 @@
+import argparse
+from events import EventManager
+from file_input import read_network
+from io import StringIO
+from contextlib import redirect_stdout
+
+parser = argparse.ArgumentParser(description='Run network simulation.')
+parser.add_argument('input_file', type=str, help='JSON input file for network')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    em = EventManager()
+    read_network(args.input_file, em)
+
+    em.run()
+
+    print(em.current_time)
+
